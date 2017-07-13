e03300493d82811052e5cecae421c3038821e976
jesuslerma <shuyojl@gmail.com>
2017-07-12 22:07:14 -0500

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,17 @@
 <template>
   <div id="app">
-    Mostrar Categorias en Tabla 
-    <input type="checkbox" 
-      @click="changeShowTable">
-    <component 
-      :is='currentCategoryView'>
-    </component>
+    <router-link to='/categories/table'> 
+      Categorias en Tabla
+    </router-link>
+    <router-link to='/categories/ul'> 
+      Categorias en Ul
+    </router-link>
   </div>
 </template>
 
 <script>
-import CategoriesUl from './components/CategoriesUl.vue'
-import CategoriesTable from './components/CategoriesTable.vue'
 
 export default {
-  name: 'app',
-  data () {
-    return {
-      showTable: false
-    }
-  },
-  computed: {
-    currentCategoryView () {
-      return this.showTable ? 'categoriesTable' : 'categoriesUl'
-    }
-  },
-  methods: {
-    changeShowTable() {
-      this.showTable = !this.showTable
-    }
-  },
-
-  components: {
-    categoriesUl: CategoriesUl,
-    categoriesTable: CategoriesTable
-  }
+  name: 'app'
 }
 </script>

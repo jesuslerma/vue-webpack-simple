bba9af592d3e49135cd18d1b9c72b1da12b87e30
jesuslerma <shuyojl@gmail.com>
2017-07-12 21:17:11 -0500

    add first category related component

---
+++ src/components/CategoriesUl.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for='category in categories'>
+      <category-li
+        :name='category.name'
+        :selected='category.selected'></category-li>
+    </li>
+  </ul>
+</template>
+<script >
+  import CategoryLi from './CategoryLi.vue'
+
+  export default {
+    props: ['categories'],
+    components: {
+      categoryLi: CategoryLi
+    }
+  }
+</script>
